12e28ef1b7636b4db88c38085c16c1311caf790b
R GNANAGURU <R GNANAGURU@DESKTOP-F1O63Q4>
2021-09-15 13:28:58 +0530

    modified passenger.java

--- LTIAirlines/src/main/java/com/lti/model/Passenger.java
+++ LTIAirlines/src/main/java/com/lti/model/Passenger.java
@@ -3,5 +3,23 @@ package com.lti.model;
 public class Passenger {
 	int passengerId;
 	String name;
+	public Passenger(int passengerId, String name) {
+		super();
+		this.passengerId = passengerId;
+		this.name = name;
+	}
+	public int getPassengerId() {
+		return passengerId;
+	}
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
